c64e20494e313e9e47f7d2e714a78cfb722f1c15
bigtimedreams <clint.taylor@scwellness.info>
2025-08-11 22:32:19 -0400

    feat: static export (no SSR) to remove large function

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This rewrite rule serves the admin panel's HTML file for the /admin route.
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-      },
-    ];
-  },
+  // Export a static site (no server function)
+  output: "export",
 
-  // This is the latest standard for configuring remote images.
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'v5.airtableusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      // You can add other domains here in the future if needed
-    ],
-  },
+  // Next/Image needs this when exporting static HTML
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
